ebecd01178bd7f7b672721a4eb878aee138150de
LeandroFMLeal <leandrofmendesleal@gmail.com>
2018-03-07 20:32:22 -0300

    eslint configuration for windows and mac

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -4,49 +4,48 @@ import FaUser from 'react-icons/lib/fa/user'
 import { Link } from 'react-router-dom'
 import './navbar.css'
 import logo from './logo-sesi.png'
-import Categoria from '../categoria/categoria'
 
-const Navbar = (props) => (
+const Navbar = () => (
     
-    <nav className="navbar">
-        <Link className="navbar-logo" to="/"> <img src={logo} alt="logo" class="logo"/></Link>
+	<nav className="navbar">
+		<Link className="navbar-logo" to="/"> <img src={logo} alt="logo" className="logo"/></Link>
 
 
-        <input id="menu-hamburguer" className="navbar-menu-input" type="checkbox" hidden />
-        <label className="navbar-menu-label" htmlFor="menu-hamburguer"><FaBars class="navbar-menu-hamburguer" /></label>
-        <ul className="navbar-pages">
-            <li>
-                <Link className="navbar-pages__link" to="/"> Home </Link>
-            </li>
+		<input id="menu-hamburguer" className="navbar-menu-input" type="checkbox" hidden />
+		<label className="navbar-menu-label" htmlFor="menu-hamburguer"><FaBars class="navbar-menu-hamburguer" /></label>
+		<ul className="navbar-pages">
+			<li>
+				<Link className="navbar-pages__link" to="/"> Home </Link>
+			</li>
            
-            <li>
+			<li>
                 
-                <Link className="navbar-pages__link" to="/cursos"> Cursos </Link>
-            </li>
+				<Link className="navbar-pages__link" to="/cursos"> Cursos </Link>
+			</li>
 
-             <li>
+			<li>
                 
-                <Link className="navbar-pages__link" to="/noticias"> Notícias </Link>
-            </li>
+				<Link className="navbar-pages__link" to="/noticias"> Notícias </Link>
+			</li>
 
-             <li>
+			<li>
                 
-                <Link className="navbar-pages__link" to="/agenda"> Agenda </Link>
-            </li>
+				<Link className="navbar-pages__link" to="/agenda"> Agenda </Link>
+			</li>
 
-             <li>
+			<li>
                 
-                <Link className="navbar-pages__link" to="/forum"> Fórum </Link>
-            </li>  
+				<Link className="navbar-pages__link" to="/forum"> Fórum </Link>
+			</li>  
            
-        </ul>
+		</ul>
 
-        <div className="navbar-pages__link">| </div> 
+		<div className="navbar-pages__link">| </div> 
              
-             <div className="navbar-pages__link">
-             <FaUser/>
-             </div>
-    </nav>
+		<div className="navbar-pages__link">
+			<FaUser/>
+		</div>
+	</nav>
 
    
 )
