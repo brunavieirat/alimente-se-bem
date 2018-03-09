055ef346af4a54a86d8a66c870c89cf7bebe902c
LeandroFMLeal <leandrofmendesleal@gmail.com>
2018-03-08 21:20:02 -0300

    screens renamed to containers

---
+++ src/containers/Login/Login.js
@@ -0,0 +1,26 @@
+import React from 'react'
+// import FacebookLogin from 'react-facebook-login'
+
+
+
+
+
+// const responseFacebook = (response) => {
+// 	console.log(response)
+// }
+   
+// const FacebookButton = () => (
+// 	<FacebookLogin
+// 		appId="377964399342322"
+// 		autoLoad={true}
+// 		fields="name,email,picture"
+// 		// onClick={props.componentClicked}
+// 		callback={responseFacebook} />,
+// 	document.getElementById('demo')
+// )
+
+
+
+export default class Login {
+    
+}
