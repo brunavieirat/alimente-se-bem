7bed2771f47f1d88054f3bf0f76ff4b1909b3072
LeandroFMLeal <leandrofmendesleal@gmail.com>
2018-03-08 21:29:30 -0300

    bug login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react'
-import FacebookButton from './components/Login/Login'
+// import FacebookButton from './components/Login/Login'
 // import {Route } from 'react-router-dom'
 // import Home from '../screens/Home'
 // import Videos from '../screens/Videos'
@@ -15,7 +15,7 @@ class App extends Component {
 		return (
 			<Fragment>
 				<Navbar />
-				<FacebookButton />
+				{/* <FacebookButton /> */}
 				{/* <Route exact path="/"  component={Home}/>
 				<Route path="/videos" component={Videos}  />
 				<Route path="/noticias" component={News} />
--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,26 +1,26 @@
-import React from 'react'
-// import FacebookLogin from 'react-facebook-login'
+// import React from 'react'
+// // import FacebookLogin from 'react-facebook-login'
 
 
 
 
 
-// const responseFacebook = (response) => {
-// 	console.log(response)
-// }
+// // const responseFacebook = (response) => {
+// // 	console.log(response)
+// // }
    
-// const FacebookButton = () => (
-// 	<FacebookLogin
-// 		appId="377964399342322"
-// 		autoLoad={true}
-// 		fields="name,email,picture"
-// 		// onClick={props.componentClicked}
-// 		callback={responseFacebook} />,
-// 	document.getElementById('demo')
-// )
+// // const FacebookButton = () => (
+// // 	<FacebookLogin
+// // 		appId="377964399342322"
+// // 		autoLoad={true}
+// // 		fields="name,email,picture"
+// // 		// onClick={props.componentClicked}
+// // 		callback={responseFacebook} />,
+// // 	document.getElementById('demo')
+// // )
 
 
 
-export default class Login {
+// export default class Login {
     
-}
+// }
