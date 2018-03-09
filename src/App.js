e29a14c94a02d45f67b504a20b81bd8de35634c0
LeandroFMLeal <leandrofmendesleal@gmail.com>
2018-03-08 21:18:44 -0300

    screens

--- src/App.js
+++ src/App.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom'
-import { BrowserRouter } from 'react-router-dom'
-import SimpleAppBar from './components/navbar/index'
-// import Button from './components/button-search/buttonSearch'
-// import Categoria from './components/categoria/categoria'
-
-
-// import './App.css';
-// import Navbar from './components/navbar/index'
-
-class App extends Component {
-  render() {
-    return (
-      
-      <BrowserRouter>
-        {/* <App /> */}
-        {/*<SimpleAppBar/>*/}
-       
-        
-        {/* <Switch>
-            <Route exact path="/"  />
-            <Route path="/"  />
-            <Route path="/" />
-            <Route path="/"  />
-        </Switch> */}
-
-      </BrowserRouter>
-     
-    );
-  }
-=======
 import React, { Component, Fragment } from 'react'
 import FacebookButton from './components/Login/Login'
 // import {Route } from 'react-router-dom'
@@ -56,7 +23,6 @@ class App extends Component {
 			</Fragment>
 		)
 	}
->>>>>>> 58d7617d9aa101b905344051bca1dc1495cfcf45
 }
 
 export default App
