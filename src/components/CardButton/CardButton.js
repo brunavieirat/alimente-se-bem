243829c89d1339e4fb83e2847f005b101233e299
LeandroFMLeal <leandrofmendesleal@gmail.com>
2018-03-13 17:46:29 -0300

    ...

--- src/components/CardButton/CardButton.js
+++ src/components/CardButton/CardButton.js
@@ -2,17 +2,23 @@ import React from 'react'
 import Paper from 'material-ui/Paper'
 
 const style = {
-	height: 100,
-	width: 100,
-	margin: 20,
-	textAlign: 'center',
-	display: 'inline-block',
+	height: 200,
+	width: 200,
+	margin: 20,	
+	display: 'flex',
+	
+}
+
+const styleDiv = {
+	display: 'flex',
+	flexDirection: 'row',
+	justifyContent: 'space-beteween',
 }
 
 const CardButton = () => (
-	<div>
+	<div style={styleDiv} >
 		
-		<Paper style={style} zdepth={1} />
+		<Paper style={style} zdepth={1}> Teste </Paper>
 		<Paper style={style} zdepth={2} />
 		<Paper style={style} zdepth={3} />
 		<Paper style={style} zdepth={4} />
--- src/containers/Forum/Forum.js
+++ src/containers/Forum/Forum.js
@@ -1,23 +1,32 @@
 import React, { Fragment, Component } from 'react'
 import PageTitle from '../../components/PageTitle/PageTitle'
 import CardButton from '../../components/CardButton/CardButton'
+import bgImage from '../../assets/images/image-05.jpeg'
 import './Forum.css'
 
-export default class Forum extends Component {
 
+export default class Forum extends Component {
 	constructor(props){
 		super(props)
 		this.state = {
 			pageTitleColor: {
 				background: '#ADC837',
+			},
+			sectionStyle: {
+				backgroundPosition: 'center',
+				backgroundRepeat: 'no-repeat',
+				backgroundSize: 'cover',
+				backgroundImage: `url(${bgImage})`,
+				height: '800px',
 			}
+			
 		}
 	}
 	render(){
 		
 		return(
 			<Fragment>
-				<section className="forum-section">
+				<section style={this.state.sectionStyle} className="forum-section">
 					<PageTitle style={this.state.pageTitleColor} title="Forum" />
 					<CardButton />
 				</section>
