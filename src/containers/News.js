dc36cfeb9c3fcd8d754fcbc4daeb40de01fddfbd
LeandroFMLeal <leandrofmendesleal@gmail.com>
2018-03-13 11:37:24 -0300

    Removing unecessary card component.

--- src/containers/News.js
+++ src/containers/News.js
@@ -1,6 +1,5 @@
 import React, { Fragment, Component } from 'react'
 import PageTitle from '../components/PageTitle/PageTitle'
-import CardButton from '../components/CardButton/CardButton'
 
 export default class News extends Component {
 	constructor(props){
