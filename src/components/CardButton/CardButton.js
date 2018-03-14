f2cd842df78a4274b4e68796a4b15e10eb3669f8
LeandroFMLeal <leandrofmendesleal@gmail.com>
2018-03-14 19:25:47 -0300

    Transfering cardbutton css to css file

--- src/components/CardButton/CardButton.js
+++ src/components/CardButton/CardButton.js
@@ -1,28 +1,16 @@
 import React from 'react'
 import Paper from 'material-ui/Paper'
+import './CardButton.css'
 
-const style = {
-	height: 200,
-	width: 200,
-	margin: 20,	
-	display: 'flex',
-	
-}
 
-const styleDiv = {
-	display: 'flex',
-	flexDirection: 'row',
-	justifyContent: 'space-beteween',
-}
 
-const CardButton = () => (
-	<div style={styleDiv} >
-		
-		<Paper style={style} zdepth={1}> Teste </Paper>
-		<Paper style={style} zdepth={2} />
+const CardButton = (props) => (
+	<div className="card-container" >
+		<Paper className="card-button" zdepth={1}> {props.title} </Paper>
+		{/* <Paper style={style} zdepth={2} />
 		<Paper style={style} zdepth={3} />
 		<Paper style={style} zdepth={4} />
-		<Paper style={style} zdepth={5} />
+		<Paper style={style} zdepth={5} /> */}
 	</div>
 )
 
