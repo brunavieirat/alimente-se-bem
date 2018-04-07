6ce645ff979de8fb5e34c239dabc0e26e5ffba88
LeandroFMLeal <leandrofmendesleal@gmail.com>
2018-04-06 21:17:17 -0300

    fix no render home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,26 +1,17 @@
 import React, { Component, Fragment } from 'react'
 import { Carousel } from 'antd'
-// import DoLogin from '../services/DoLogin'
 
-
-// function DoTryLogin(props){
-// 	if(props.isLogged){
-// 		return <DoLogin />
-// 	}
-// 	return  null
-// }
 class Home extends Component{
 
 	render(){
 		return(
 			<Fragment>
-				{/* <DoTryLogin isLogged={true} /> */}
-				{/* <Carousel autoplay>
+				<Carousel autoplay>
 					<div><h3>1</h3></div>
 					<div><h3>2</h3></div>
 					<div><h3>3</h3></div>
 					<div><h3>4</h3></div>
-				</Carousel> */}
+				</Carousel>
 			</Fragment>
 		)
 	}
